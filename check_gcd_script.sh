63a34583e3c09bd20e37d8fba86d4b5136c729a0
kyoko yoshida <faultier.faultier@gmail.com>
2022-08-07 04:00:49 +0900

    update some tests

--- check_gcd_script.sh
+++ check_gcd_script.sh
@@ -12,25 +12,24 @@ ERROR_EXIT () {
 
 #引数が足りない
 ./gcd.sh 2> $tmp-ans && ERROR_EXIT "error in test1-1"
-diff $tmp-ans  $tmo-args || ERROR_EXIT "error in test1-2"
+diff $tmp-ans  $tmp-args || ERROR_EXIT "error in test1-2"
 
 #引数が多い
 ./gcd.sh 4 5 2> $tmp-ans && ERROR_EXIT "error in test2-1"
-diff $tmp-ans  $tmo-args || ERROR_EXIT "error in test2-2"
+diff $tmp-ans  $tmp-args || ERROR_EXIT "error in test2-2"
 
 #引数が文字
 ./gcd.sh a b> $tmp-ans && ERROR_EXIT "error in test3-1"
-diff $tmp-ans  $tmo-nat || ERROR_EXIT "error in test3-2"
+diff $tmp-ans  $tmp-nat || ERROR_EXIT "error in test3-2"
 
 
 #引数がマイナスの値
 ./gcd.sh -1 1> $tmp-ans && ERROR_EXIT "error in test4-1"
-diff $tmp-ans  $tmo-nat || ERROR_EXIT "error in test4-2"
+diff $tmp-ans  $tmp-nat || ERROR_EXIT "error in test4-2"
 
 
 #引数が小数
 ./gcd.sh 1 2.5> $tmp-ans && ERROR_EXIT "error in test5-1"
-diff $tmp-ans  $tmo-nat || ERROR_EXIT "error in test5-2"
+diff $tmp-ans  $tmp-nat || ERROR_EXIT "error in test5-2"
 
-
-
+rm /tmp/$tmp-*
