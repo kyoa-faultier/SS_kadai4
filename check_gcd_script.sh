df15b9d56d2862020834582c0198e3d5ae61be13
kyoko yoshida <faultier.faultier@gmail.com>
2022-08-07 10:26:55 +0900

    update yaml

--- check_gcd_script.sh
+++ check_gcd_script.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-tmp=/tmp/$$
-echo "入力できる自然数は「２つ」です" > $tmp-args
-echo "「自然数」を２つ入力してください" > $tmp-nat
+arg="/tmp/$$-arg"
+nat="tmp/$$-nat"
+result="/tmp/$$-result"
 
-ERROR_EXIT () {
-	echo "$1" >&2
-	rm -f $tmp-*
+echo "入力できる自然数は「２つ」です" > ${arg}
+echo "「自然数」を２つ入力してください" > ${nat}
+
+ERROR_EXIT(){
+	echo $1
+	rm /tmp/$$-*
 	exit 1
 }
 
 #引数が足りない
-./gcd.sh 2> $tmp-ans && ERROR_EXIT "error in test1-1"
-diff $tmp-ans  $tmp-args || ERROR_EXIT "error in test1-2"
+./gcd.sh 2> ${result}
+diff ${arg}  ${result} || ERROR_EXIT "error in test1"
 
 #引数が多い
-./gcd.sh 4 5 2> $tmp-ans && ERROR_EXIT "error in test2-1"
-diff $tmp-ans  $tmp-args || ERROR_EXIT "error in test2-2"
+./gcd.sh 4 5 2> ${result}
+diff ${arg} ${result} || ERROR_EXIT "error in test2"
 
 #引数が文字
-./gcd.sh a b> $tmp-ans && ERROR_EXIT "error in test3-1"
-diff $tmp-ans  $tmp-nat || ERROR_EXIT "error in test3-2"
+./gcd.sh a b> ${result}
+diff ${nat} ${result} || ERROR_EXIT "error in test3"
 
 
 #引数がマイナスの値
-./gcd.sh -1 1> $tmp-ans && ERROR_EXIT "error in test4-1"
-diff $tmp-ans  $tmp-nat || ERROR_EXIT "error in test4-2"
+./gcd.sh -1 1> ${result}
+diff ${nat} ${result} || ERROR_EXIT "error in test4"
 
 
 #引数が小数
-./gcd.sh 1 2.5> $tmp-ans && ERROR_EXIT "error in test5-1"
-diff $tmp-ans  $tmp-nat || ERROR_EXIT "error in test5-2"
+./gcd.sh 1 2.5> ${result}
+diff ${nat} ${result} || ERROR_EXIT "error in test5"
 
 rm /tmp/$tmp-*
